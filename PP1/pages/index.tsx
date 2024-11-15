db08fa0b30cf462deb415266cc9b6e5bb1434f72
Riixardo <RicardoMonriguez2@gmail.com>
2024-11-14 22:53:54 -0500

    added typescript to project

---
+++ PP1/pages/index.tsx
@@ -0,0 +1,35 @@
+const Home = () => {
+  return (
+    <div className="min-h-screen w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+      <div className="flex items-center justify-center">
+        Hello
+      </div>
+      <div className="flex items-center justify-center">
+        Hello
+      </div>
+      <div className="flex items-center justify-center">
+        Hello
+      </div>
+      <div className="flex items-center justify-center">
+        Hello
+      </div>
+      <div className="flex items-center justify-center">
+        Hello
+      </div>
+      <div className="flex items-center justify-center">
+        Hello
+      </div>
+      <div className="flex items-center justify-center">
+        Hello
+      </div>
+      <div className="flex items-center justify-center">
+        Hello
+      </div>
+      <div className="flex items-center justify-center">
+        Hello
+      </div>
+    </div>
+  )
+}
+
+export default Home;
