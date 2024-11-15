200d6e3f225ecf677b2499153bdb7e08a44eed51
Riixardo <RicardoMonriguez2@gmail.com>
2024-11-14 23:03:47 -0500

    added a default footer

--- PP1/pages/_document.tsx
+++ PP1/pages/_document.tsx
@@ -7,6 +7,10 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+        <footer className="flex justify-between w-full h-10 bg-black text-white">
+          <div className="ml-4">Copyright 2024</div>
+          <div className="mr-4">CSC309 Group 8068</div>
+          </footer>
       </body>
     </Html>
   );
--- PP1/pages/index.tsx
+++ PP1/pages/index.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="min-h-screen w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+    <main className="min-h-screen w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
       <div className="flex items-center justify-center">
         Hello
       </div>
@@ -28,7 +28,7 @@ const Home = () => {
       <div className="flex items-center justify-center">
         Hello
       </div>
-    </div>
+    </main>
   )
 }
 
